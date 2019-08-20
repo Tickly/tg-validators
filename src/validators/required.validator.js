ff7f5d987fdb80e7bff22b38f41ac9ed40c78f58
taoguo <807081817@qq.com>
2019-08-20 10:15:13 +0800

    优化required

--- src/validators/required.validator.js
+++ src/validators/required.validator.js
@@ -2,25 +2,24 @@ import Validator from './base.validator'
 
 export default class RequiredValidator extends Validator {
 
-  constructor(options) {
+  constructor (options) {
     super(options);
 
     this.message = '{attribute}不能为空';
   }
 
-  validateValue(value) {
+  validateValue (value) {
     do {
       if (0 === value) {
         break;
       }
 
       // 如果是有元素的数组
-      if (Array.isArray(value)) {
-        if (value.length > 0) break;
-      } else {
-        // 如果有值
-        if (!!value) break;
-      }
+      if (Array.isArray(value) && value.length > 0) break;
+      // 布尔型通过验证
+      if ('boolean' === typeof value) break
+      // 如果有值
+      if (!!value) break;
 
       return this.message
     } while (false);
